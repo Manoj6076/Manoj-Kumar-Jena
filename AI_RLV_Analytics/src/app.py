32ac08dd72357b8d22363e3b7492cf47e31601cf
Manoj6076 <mjena0921@gmail.com>
2026-03-12 13:20:43 +0530

    Update app.py

--- AI_RLV_Analytics/src/app.py
+++ AI_RLV_Analytics/src/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title("AI RLV Anomaly Detection")
-
-data = pd.read_csv("data/telemetry.csv")
+data = pd.read_csv("../data/telemetry.csv")
 
 st.write(data.head())
+
