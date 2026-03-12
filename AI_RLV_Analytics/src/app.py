b1333f71c6d46f35245cea21d84e4b7204665d40
Manoj6076 <mjena0921@gmail.com>
2026-03-12 13:38:50 +0530

    Update app.py

--- AI_RLV_Analytics/src/app.py
+++ AI_RLV_Analytics/src/app.py
@@ -1,16 +1,8 @@
 import streamlit as st
 import pandas as pd
-import os
 
 st.title("RLV Telemetry Anomaly Analytics")
 
-# Get project root directory
-BASE_DIR = os.getcwd()
+data = pd.read_csv("AI_RLV_Analytics/data/rlv_telemetry.csv")
 
-# Dataset path
-data_path = os.path.join(BASE_DIR, "data", "telemetry.csv")
-
-# Read dataset
-data = pd.read_csv(data_path)
-
-st.dataframe(data.head())
+st.write(data.head())
